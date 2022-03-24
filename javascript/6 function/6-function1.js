a8b1fb10c7d0fc64b50d8711c897ddfe13fcfb85
ErickWahyudy <96457307+ErickWahyudy@users.noreply.github.com>
2022-03-24 15:00:19 +0700

    upload file tugas javascript

---
+++ javascript/6 function/6-function1.js
@@ -0,0 +1,19 @@
+// Mengembalikan teks yang berkebalikan
+//
+// contoh: 
+// teks = hello world
+// hasil:
+// dlrow olleh
+
+function reverseString(str) {
+  var newString = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+}
+
+const string = prompt('Masukan teks: ');
+
+const result = reverseString(string);
+console.log(result);
---
+++ javascript/function.js
@@ -0,0 +1,22 @@
+// contoh 1: deklarasi fungsi dengan nama greet()
+//function greet() {
+    //console.log('Hello, world!');
+  //}
+  
+  // pemanggilan fungsi
+  //greet();
+
+  function greet() {
+    console.log('Hello, world!');
+  }
+  
+  function welcome() {
+    console.log('Welcome to JS function');
+  }
+  
+  function callGreeting() {
+    greet();
+    welcome();
+  }
+  
+  callGreeting();
