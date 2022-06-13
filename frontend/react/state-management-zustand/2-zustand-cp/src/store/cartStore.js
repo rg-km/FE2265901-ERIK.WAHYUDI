6219ec8b877f7c48702509c0509348c246cda70e
ErickWahyudy <erickwahyudy@gmail.com>
2022-06-13 08:41:56 +0700

    update syncr

--- frontend/react/state-management-zustand/2-zustand-cp/src/store/cartStore.js
+++ frontend/react/state-management-zustand/2-zustand-cp/src/store/cartStore.js
@@ -1,17 +1,82 @@
-import create from "zustand"
+import create from "zustand";
+import { immer } from "zustand/middleware/immer";
+import { useStore } from "zustand";
 // TODO: answer here
 
 const cartQuantityMiddleware = (config) => (set, get, api) =>
   config(
+    (nextState, action) => {
+      if (action.type === "ADD_TO_CART") {
+        nextState.cart.push(action.payload);
+      } else if (action.type === "REMOVE_FROM_CART") {
+        nextState.cart = nextState.cart.filter(
+          (item) => item.id !== action.payload
+        );
+      } else if (action.type === "CHANGE_QUANTITY") {
+        nextState.cart = nextState.cart.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, quantity: action.payload.quantity }
+            : item
+        );
+      }
+    },
+    (setState, getState) => {
+      setState((state) => {
+        state.cart = getState().cart;
+      });
+    }
+  );
+  
+
     // TODO: answer here
-    ,
+    (args) => {
+      const moreThanStockItems = args.items.filter(
+        (item) => item.quantity > item.stock
+      );
+
+      const lessThanZeroItems = args.items.filter((item) => item.quantity < 1);
+
+      if (moreThanStockItems.length > 0) {
+        throw new Error(`tIDAK BOLEH MELEBIHI STOCK`);
+      }
+      if (lessThanZeroItems.length > 0) {
+        throw new Error(`tIDAK BOLEH KURANG DARI NOL`);
+      }
+    },
     get,
-    api,
-  )
+    api
+  );
 
-const useCartStore = 
+const useCartStore = create;
 // TODO: answer here
+immer(
+  cartQuantityMiddleware((set) => ({
+    items: [],
+    addItem: (item) =>
+      set((state) => ({
+        items: [...state.items, item],
+      })),
+    removeItem: (item) =>
+      set((state) => ({
+        items: state.items.filter((i) => i !== item),
+      })),
+  })),
 
+  (set) => ({
+    items: [],
+    addItem: (item) =>
+      set((state) => ({
+        items: [...state.items, item],
+      })),
+    removeItem: (item) =>
+      set((state) => ({
+        items: state.items.filter((i) => i !== item),
+      })),
+    changeQuantity: (item, quantity) =>
+      set((state) => ({
+        items: state.items.map((i) => (i === item ? { ...i, quantity } : i)),
+      })),
+  })
+);
 
-
-export default useCartStore
+export default useCartStore;
--- frontend/react/state-management-zustand/2-zustand-cp/src/store/itemStore.js
+++ frontend/react/state-management-zustand/2-zustand-cp/src/store/itemStore.js
@@ -1,7 +1,29 @@
 import create from "zustand"
+import { persist, devtools } from "zustand/middleware"
+import { produce } from "immer"
+import { useEffect } from "react"
+import { useCartStore } from "./cartStore"
+import { useUserStore } from "./userStore"
 // TODO: answer here
 
 const useItemStore = 
+    create(persist(devtools((set) => ({
+        items: [],
+        // whitout immer
+        // addItem: (item) => set((state) => ({ ...state, items: [...state.items, item] })),
+        // with immer
+        addItem: (item) => set(produce((state) => {
+            state.items.push(item)
+        })),
+        removeItem: (item) => set(produce((state) => {
+            state.items = state.items.filter(i => i.id !== item.id)
+        })),
+        changeQuantity: (item) => set(produce((state) => {
+            const index = state.items.findIndex(i => i.id === item.id)
+            state.items[index].quantity = item.quantity
+        })),
+    }))),{name: "item"});
+    
 // TODO: answer here
 
 export default useItemStore
